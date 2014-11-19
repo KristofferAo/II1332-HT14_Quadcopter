a3d572961452f0cf6819f312d5845cfcf077f260
Joel Abrahamsson <joel.abrahamsson1@gmail.com>
2014-11-19 12:47:47 +0100

    Lägg till src och inc mappar i Kod
    Lägg till temporära källkodsfiler i src och inc

---
+++ Kod/inc/main.h
@@ -0,0 +1,8 @@
+/*
+ *  main.h PLACEHOLDER - THIS IS A TEMPORARY FILE!
+ */
+
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#endif
---
+++ Kod/src/main.c
@@ -0,0 +1,10 @@
+/*
+ *  main.c PLACEHOLDER - THIS IS A TEMPORARY FILE!
+ */
+
+#include "main.h"
+
+int main()
+{
+  return 0;
+}
